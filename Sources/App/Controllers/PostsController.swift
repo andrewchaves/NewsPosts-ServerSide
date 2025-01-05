c3361e3ecd19d2a849d52836228d83b1a133aa09
Andrew Chaves <andrew.almeidachaves@gmail.com>
2025-01-04 22:22:24 -0400

    Variable names adjustments

--- Sources/App/Controllers/PostsController.swift
+++ Sources/App/Controllers/PostsController.swift
@@ -3,35 +3,35 @@ import Vapor
 
 struct PostsController: RouteCollection {
     func boot(routes: RoutesBuilder) throws {
-        let todos = routes.grouped("posts")
-
-        todos.get(use: self.index)
-        todos.post(use: self.create)
-        todos.group(":postID") { todo in
-            todo.delete(use: self.delete)
+        let posts = routes.grouped("posts")
+        
+        posts.get(use: self.index)
+        posts.post(use: self.create)
+        posts.group(":postID") { post in
+            post.delete(use: self.delete)
         }
     }
-
+    
     @Sendable
     func index(req: Request) async throws -> [PostDTO] {
         try await Post.query(on: req.db).all().map { $0.toDTO() }
     }
-
+    
     @Sendable
     func create(req: Request) async throws -> PostDTO {
-        let todo = try req.content.decode(PostDTO.self).toModel()
-
-        try await todo.save(on: req.db)
-        return todo.toDTO()
+        let post = try req.content.decode(PostDTO.self).toModel()
+        
+        try await post.save(on: req.db)
+        return post.toDTO()
     }
-
+    
     @Sendable
     func delete(req: Request) async throws -> HTTPStatus {
-        guard let todo = try await Post.find(req.parameters.get("postID"), on: req.db) else {
+        guard let post = try await Post.find(req.parameters.get("postID"), on: req.db) else {
             throw Abort(.notFound)
         }
-
-        try await todo.delete(on: req.db)
+        
+        try await post.delete(on: req.db)
         return .noContent
     }
 }
