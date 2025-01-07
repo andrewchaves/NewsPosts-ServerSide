00b37ddc52d91fb02e92e867bb1df5ae13860c0c
Andrew Chaves <andrew.almeidachaves@gmail.com>
2025-01-07 16:58:35 -0400

    Resolve other conflict issues

--- Sources/App/Models/Post.swift
+++ Sources/App/Models/Post.swift
@@ -5,11 +5,8 @@ import struct Foundation.UUID
 /// It is recommended you write your model with sendability checking on and then suppress the warning
 /// afterwards with `@unchecked Sendable`.
 final class Post: Model, @unchecked Sendable {
-<<<<<<< HEAD
-    static let schema = "Posts"
-=======
+
     static let schema = "posts"
->>>>>>> 77a25e7 (Variable names adjustments)
     
     @ID(key: .id)
     var id: UUID?
@@ -25,22 +22,15 @@ final class Post: Model, @unchecked Sendable {
     init(id: UUID? = nil, headline: String, content: String) {
         self.id = id
         self.headline = headline
-<<<<<<< HEAD
-=======
         self.content = content
->>>>>>> 77a25e7 (Variable names adjustments)
+
     }
     
     func toDTO() -> PostDTO {
         .init(
             id: self.id,
-<<<<<<< HEAD
-            headline: self.$headline.value, // ADIÇÃO DO $ Foi sugerida pelo compilador, não entendi o porquê
-            content: self.$content.value // ADIÇÃO DO $ Foi sugerida pelo compilador, não entendi o porquê
-=======
             headline: self.headline, 
             content: self.content
->>>>>>> 77a25e7 (Variable names adjustments)
         )
     }
 }
